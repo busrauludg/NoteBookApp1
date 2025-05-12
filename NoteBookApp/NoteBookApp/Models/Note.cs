f3ce2f35e99ef5c54ca2be3687e47ca97ee5f2bb
busrauludg <busrapc.226@gmail.com>
2025-05-12 17:35:31 +0300

    Eski projeyi geri yükledim

---
+++ NoteBookApp/NoteBookApp/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿namespace NoteBookApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = new DateTime(
+         DateTime.Now.Year,
+         DateTime.Now.Month,
+         DateTime.Now.Day,
+         DateTime.Now.Hour,
+         DateTime.Now.Minute,
+         DateTime.Now.Second
+        );
+
+        public DateTime? UpdateDate { get; set; }//// Nullable olmalı, çünkü başlangıçta update yapılmamış olabilir.
+    }
+}
